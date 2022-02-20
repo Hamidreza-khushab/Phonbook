6089afa6290791b48aa5f62a3f2cac63c2e56359
Hamidreza <qiisohamid@gmail.com>
2022-02-20 12:31:48 +0100

    add get function and query get command

--- servre.js
+++ servre.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { async } = require('regenerator-runtime');
 mongoose.connect('mongodb://localhost:27017/testPhoneBook').then(() => {console.log('db connected');}).catch((err) => {console.log('db not connected', err);});
 
 const schemaPhonenumber = new mongoose.Schema({
@@ -21,4 +22,33 @@ async function createNumber()
     const savedNumber = await maxPhonenumber.save();
     console.log('Number Saved', savedNumber );
 }
-createNumber();
+// Filter parameters
+// eq => equal to
+// ne => not equal to
+// gt => greater than
+// gte => greater than or equal to
+// lt => 
+// lte
+// in
+// nin => mot in
+
+async function getPhonenumberList()
+{
+    // const phoneNumberList = await PhonebookModel.find();
+    // const phoneNumberList = await PhonebookModel.find({ name: 'max' });
+    // const phoneNumberList = await PhonebookModel.find({ phoneNumber:{ $ne : 12345  } });
+    // const phoneNumberList = await PhonebookModel.find({ phoneNumber:{ $nin : [ 12345 ] } });
+    // const phoneNumberList = await  PhonebookModel.find().or([ { name:'jak' }, { phoneNumber : { $lt :20000 } } ]);  
+    const phoneNumberList = await  PhonebookModel.find().and([ { name:'jak' }, { phoneNumber : { $lt :20000 } } ]);    
+    // const phoneNumberList = await PhonebookModel.find().limit(1);
+    // const phoneNumberList = await PhonebookModel.find().sort({ name:-1 });
+    // const phoneNumberList = await PhonebookModel.find().sort({ name:-1 }).select({ 
+    //     name : 1,
+    //     phoneNumber:1
+    // });
+    console.clear();
+    console.log(phoneNumberList);
+}
+
+// createNumber();
+getPhonenumberList();
